8cb8748ee34e261ca942da423e1ed2a1236ccc09
MarcinZbor <marcinzb@gazeta.pl>
2023-05-16 17:50:57 +0200

    ReadyforCss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,66 @@
-import React, { Component } from 'react';
-import css from './App.module.css';
+import React from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactsList from './ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+import styles from './App.module.css';
 
-class App extends Component {
+ class App extends React.Component {
   state = {
-    name: '',
-    contact: ''
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleInputChange = (e) => {
-    this.setState({
-      name: e.target.value
-    });
+  handleSubmit = event => {
+    event.preventDefault();
+    const name = event.currentTarget.elements.name.value;
+    const number = event.currentTarget.elements.number.value;
+    const newContact = { name: name, id: nanoid(), number: number };
+    this.state.contacts.some(contact => name === contact.name)
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+    event.currentTarget.elements.name.value = '';
+    event.currentTarget.elements.number.value = '';
   };
 
-  handleButtonClick = () => {
-    this.setState({
-      contact: this.state.name,
-      name: ''
-    });
+  handleSearch = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  handleFilter = () =>
+    this.state.filter
+      ? this.state.contacts.filter(contact =>
+          contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+        )
+      : this.state.contacts;
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
 
   render() {
     return (
-      <>
-        <form className={css.form}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            onChange={this.handleInputChange}
-            value={this.state.name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <button type="button" onClick={this.handleButtonClick}>
-            Add contact
-          </button>
-        </form>
-        <h3>Contacts</h3>
-        <p>{this.state.contact}</p>
-      </>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm   formSubmit={this.handleSubmit} />
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter inputSearch={this.handleSearch} />
+        <ContactsList
+          contacts={this.handleFilter()}
+          onDelete={this.handleDelete}
+        />
+      </div>
     );
   }
 }
 
-export default App;
+export default App
